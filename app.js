24bc93c5b8f1340b4a9ed64689c87080769b0e2d
Gonzalo Ronderos <gonzalorond@gmail.com>
2022-02-10 16:34:03 -0300

    sitio web terminado

--- app.js
+++ app.js
@@ -3,17 +3,53 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+app.use(express.static("public"));
+
 app.listen(3030, () => {
     console.log("El servidor funciona bien en 3030")
 });
 
 app.get('/home', (req, res) => {
     //res.sendFile("C:/Users/gonza/OneDrive/Escritorio/AhoraSi/DHHeroes/views/index.html"); // este es con ruta completa
-    res.sendFile(path.join(__dirname, "./views/index.html")); //esta es con ruta relativa
+    const fileToSend = path.join(__dirname, "./views/index.html");
+    res.sendFile(fileToSend);
 });
 
-/*let htmlPath = path.resolve(__dirname, './views/index.html');
-console.log(htmlPath)*/
+app.get('/babbage', (req, res) => {
+    const fileToSend = path.join(__dirname, "./views/babbage.html")
+    res.sendFile(fileToSend); 
+});
+
+app.get('/berners-lee', (req, res) => {
+    const fileToSend = path.join(__dirname, "./views/berners-lee.html")
+    res.sendFile(fileToSend)
+});
+
+app.get('/clarke', (req, res) => {
+    const fileToSend = path.join(__dirname, "./views/clarke.html")
+    res.sendFile(fileToSend)
+});
+
+app.get('/hamilton', (req, res) => {
+    const fileToSend = path.join(__dirname, "./views/hamilton.html")
+    res.sendFile(fileToSend) 
+});
+
+app.get('/hopper', (req, res) => {
+    const fileToSend = path.join(__dirname, "./views/hopper.html")
+    res.sendFile(fileToSend) 
+});
+
+app.get('/lovelace', (req, res) => {
+    const fileToSend = path.join(__dirname, "./views/lovelace.html")
+    res.sendFile(fileToSend)
+});
+
+app.get('/turing', (req, res) => {
+    const fileToSend = path.join(__dirname, "./views/turing.html")
+    res.sendFile(fileToSend) 
+});
+
 
 
 
