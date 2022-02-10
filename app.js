24f60dc0bbb764709538d746e54964bde926c6dd
Gonzalo Ronderos <gonzalorond@gmail.com>
2022-02-10 12:29:34 -0300

    se inicio servidor correctamente

---
+++ app.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.listen(3030, () => {
+    console.log("El servidor funciona bien en 3030")
+});
+
+
+
