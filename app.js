8eb87b4eacf4e447e1ae8f96e143739a1b45a520
Gonzalo Ronderos <gonzalorond@gmail.com>
2022-02-10 12:56:04 -0300

    primer pag home realizada con exito

--- app.js
+++ app.js
@@ -7,5 +7,14 @@ app.listen(3030, () => {
     console.log("El servidor funciona bien en 3030")
 });
 
+app.get('/home', (req, res) => {
+    //res.sendFile("C:/Users/gonza/OneDrive/Escritorio/AhoraSi/DHHeroes/views/index.html"); // este es con ruta completa
+    res.sendFile(path.join(__dirname, "./views/index.html")); //esta es con ruta relativa
+});
+
+/*let htmlPath = path.resolve(__dirname, './views/index.html');
+console.log(htmlPath)*/
+
+
 
 
